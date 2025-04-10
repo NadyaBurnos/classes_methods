f6a1425caf0b9778b568d3e63dfd775ba2d63689
Надежда Бурнос <eparis@atlassian.com>
2025-04-10 11:52:41 +0700

    Initial commit

--- src/characters.js
+++ src/characters.js
@@ -9,18 +9,13 @@ export default class Character {
             'Zombie',
         ]
 
-        if (name.length >= 2 && name.length <= 10) {
-            this.name = name;
-        }
-        else {
+        if (name.length < 2 || name.length > 10) {
+            throw new Error("Ошибка");
+        } else if (types.indexOf(type) === -1) {
             throw new Error("Ошибка");
         }
-        if ( types.includes(type) ) {
-            this.type = type;
-        } 
-        else {
-            throw new Error("Ошибка");
-        }
+        this.name = name;
+        this.type = type;
 
         this.health = 100;
         this.level = 1;
@@ -30,20 +25,23 @@ export default class Character {
 
     levelUp() {
 
-        if ( this.health >= 0 ) {
-            this.level++;
-            this.attack = this.attack + this.attack * 0.2;
-            this.defence = this.defence + this.defence * 0.2;
-            this.health = 100;
-        } else { throw new Error("Нельзя повысить уровень умершего") }
+        if ( this.health < 0 ) {
+            throw new Error("Нельзя повысить уровень умершего");
+        } 
+        this.level++;
+        this.attack = this.attack + this.attack * 0.2;
+        this.defence = this.defence + this.defence * 0.2;
+        this.health = 100; 
 
     }
 
     damage(points) {
 
-        if ( this.health >= 0 ) {
-            this.health -= points * ( 1 - this.defence / 100 );
-        } else { throw new Error("Нельзя наносить вред умершему") }
+        if ( this.health < 0 ) {
+            throw new Error("Нельзя наносить вред умершему");
+        }
+        this.health -= points * ( 1 - this.defence / 100 );
+        
 
     }
 }
